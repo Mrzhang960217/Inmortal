f3c4332d3d30be17adc067a265303d07ef9df331
LinoMen <1012002793@qq.com>
2016-12-06 17:41:59 +0800

    '添加后台页面'

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,13 +7,30 @@ class Index extends Controller
 	{
 		return $this->fetch();
 	}
+	
+	public function home()
+	{
+		return $this->fetch();
+	}
+
 	public function admin_index()
 	{
 		return $this->fetch();
 	}
-	public function home()
-	{
 
+	public function products_list()
+	{
+		return $this->fetch();
+	}
+
+	public function brand_Manage()
+	{
+		return $this->fetch();
+	}
+
+	public function category_manage()
+	{
+		return $this->fetch();
 	}
 }
 
